72caea8cdccffd19e1ce4faeea42ca6d35cde1db
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2024-03-01 21:26:14 +0300

    feat(enabling-cond): enabling conditions with time

--- src/action/enabling_condition.rs
+++ src/action/enabling_condition.rs
@@ -1,8 +1,17 @@
+#[allow(unused_variables)]
 pub trait EnablingCondition<State> {
     /// Enabling condition for the Action.
     ///
     /// Checks if the given action is enabled for a given state.
-    fn is_enabled(&self, #[allow(unused_variables)] state: &State) -> bool {
+    fn is_enabled(&self, state: &State) -> bool {
         true
     }
+
+    /// Enabling condition for the Action.
+    ///
+    /// Checks if the given action is enabled for a given state.
+    fn is_enabled_with_time(&self, state: &State, time: crate::Timestamp) -> bool {
+        self.is_enabled(state)
+    }
 }
+
