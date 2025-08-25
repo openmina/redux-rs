f2ef4db5cf21624988bd2fac878f7c79ecb1034e
dannywillems <dannywillems@users.noreply.github.com>
2025-08-25 17:21:02 +0000

    deploy: f31af10b917a4612e3afecc8f61a46e085aaaa96

--- src-files.js
+++ src-files.js
@@ -1,3 +1,3 @@
-var srcIndex = new Map(JSON.parse('[["redux",["",[["action",[],["action_id.rs","action_meta.rs","action_with_meta.rs","enabling_condition.rs","mod.rs"]]],["effects.rs","lib.rs","reducer.rs","service.rs","store.rs","sub_store.rs","timestamp.rs"]]]]'));
+var srcIndex = new Map(JSON.parse('[["redux",["",[["action",[],["action_id.rs","action_meta.rs","action_with_meta.rs","enabling_condition.rs","mod.rs"]]],["callback.rs","dispatcher.rs","effects.rs","instant.rs","lib.rs","reducer.rs","service.rs","store.rs","sub_store.rs","timestamp.rs"]]]]'));
 createSrcSidebar();
-//{"start":36,"fragment_lengths":[210]}
+//{"start":36,"fragment_lengths":[253]}
---
+++ trait.impl/core/ops/arith/trait.SubAssign.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["redux",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/arith/trait.SubAssign.html\" title=\"trait core::ops::arith::SubAssign\">SubAssign</a>&lt;<a class=\"struct\" href=\"https://doc.rust-lang.org/1.84.1/core/time/struct.Duration.html\" title=\"struct core::time::Duration\">Duration</a>&gt; for <a class=\"struct\" href=\"redux/struct.Instant.html\" title=\"struct redux::Instant\">Instant</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[429]}
